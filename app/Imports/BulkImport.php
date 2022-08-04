0a069716b484f061dd3b8c86087742ab6501057d
unknown <vikassingh438@gmail.com>
2022-08-04 15:06:22 +0530

    update sender

--- app/Imports/BulkImport.php
+++ app/Imports/BulkImport.php
@@ -18,13 +18,14 @@ class BulkImport implements ToModel,WithHeadingRow
     public function model(array $row)
     {
         if($_POST['import_type'] == 1){
-           //echo "<pre>"; print_r($row);die;
+        //    echo "<pre>"; print_r($row);die;dd
             $lastworkingdate = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($row['last_working_date']);
             $lastworkingdate = $lastworkingdate->format('d-m-Y');
             $sender = DB::table('sender_details')
                 ->where('ax_id', '=', $row['ax_id'])
                 ->first();
-            if(is_null($sender)) {
+               
+            if(empty($sender)) {
                 if($row['type'] == 'Employee'){
                     return new Sender([
                         'ax_id' => $row['ax_id'],
@@ -36,11 +37,12 @@ class BulkImport implements ToModel,WithHeadingRow
                         'status' => $row['status'],
                         'last_working_date' => $lastworkingdate,
                     ]);
-                }else{
-                    DB::table('sender_details')
-                    ->where('name',$row['name'])->update([ 'name' => $row['name'], 'type' => $row['type'], 'location' => $row['location'], 'telephone_no' => $row['telephone_no'] ]);
                 }
-            }
+            }else{
+                //dd('ll');
+               DB::table('sender_details')
+               ->where('ax_id', $row['ax_id'])->update(['status' => $row['status'] ]);
+           }
         }
 
         if($_POST['import_type'] == 2){
