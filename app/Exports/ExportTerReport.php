5292fdd2e912ed271df859572eb9073904ebf290
DhroovKanwar <dhroov.kanwar@eternitysolutions.net>
2022-09-23 23:46:34 +0530

    export ter pushed on 23/09/2022

--- app/Exports/ExportTerReport.php
+++ app/Exports/ExportTerReport.php
@@ -13,13 +13,46 @@ class ExportTerReport implements FromCollection, WithHeadings
      */
     public function collection()
     {
-        return Tercourier::select('id','saved_by_name','created_at','updated_by_name','updated_at')->get();
+        $data=Tercourier::select('id','created_at','saved_by_name','updated_by_name','updated_at')->get();
+        $size=sizeof($data);
+        // $val="";
+         
+        for($i=0;$i<$size;$i++)
+        {
+            $val= explode(" ",$data[$i]->created_at);
+            $date1=$val[0];
+            $time1=$val[1];
+            $id=$data[$i]->id;
+            $saved_by_name=$data[$i]->saved_by_name;
+            $updated_by_name=$data[$i]->updated_by_name;
+            $val2= explode(" ",$data[$i]->updated_at);
+            $date2=$val2[0];
+            $time2=$val2[1];
+
+            $arr_instrulist_excel[] = array(
+                'id'  => $id,
+                'saved_by_name'   => $saved_by_name,
+                'created_at'    => $date1,
+                'created_time' => $time1,
+                'updated_by_name'  => $updated_by_name,
+                'updated_at'   => $date2,
+                'updated_time' => $time2
+               );
+            
+            // echo"<pre>";
+            // return[$id,$saved_by_name,$date1,$time1,$updated_by_name,$date2,$time2];
+            // print_r($time);
+        }
+
+        return collect($arr_instrulist_excel);
+       
+        // return Tercourier::select('id','saved_by_name','created_at','updated_by_name','updated_at')->get();
     }
 
     public function headings(): array
     {
         return [
-            "ID","Created_by_user_name", "Created_at","Updated_by_user_name", "Updated_at"
+            "ID","Created_by", "Created_date","Created_time","Updated_by", "Updated_date","Updated_time"
         ];
     }
 }
