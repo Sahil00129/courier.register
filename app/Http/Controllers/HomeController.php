155a83893e790629095f07f332d548b0a1b61d99
vikaschandel <vikassingh438>
2022-10-14 10:52:50 +0530

    widgite

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tercourier;
+use Carbon\Carbon;
 use DB;
 use Response;
-use App\Models\Tercourier;
-
 
 class HomeController extends Controller
 {
@@ -45,11 +45,11 @@ class HomeController extends Controller
         $sum2 = array();
         $data_sent_to_finfect = DB::table('tercouriers')->select('payable_amount')->where('status', 3)->whereDate('updated_at', date("Y-m-d"))->get();
         // echo"<pre>";
-       if(sizeof($data_sent_to_finfect)>0){
-        $current_day_sent_to_finfect_ter_sum = self::totalSum($data_sent_to_finfect);
-       }else{
-       $current_day_sent_to_finfect_ter_sum=0;
-       }
+        if (sizeof($data_sent_to_finfect) > 0) {
+            $current_day_sent_to_finfect_ter_sum = self::totalSum($data_sent_to_finfect);
+        } else {
+            $current_day_sent_to_finfect_ter_sum = 0;
+        }
 
         /////////////////////Current Month Processed//////////////////////////
         $current_month_sent_to_finfect_ter_count = DB::table('tercouriers')->select('id')->where('status', 3)->whereMonth('updated_at', date("m"))->whereYear('updated_at', date("Y"))->count();
@@ -60,11 +60,11 @@ class HomeController extends Controller
         $data_sent_to_finfect = DB::table('tercouriers')->select('payable_amount')->where('status', 3)->whereMonth('updated_at', date("m"))->whereYear('updated_at', date("Y"))->get();
         // echo"<pre>";
         // print_r($data_sent_to_finfect);
-        if(sizeof($data_sent_to_finfect)>0){
-        $current_month_sent_to_finfect_ter_sum = self::totalSum($data_sent_to_finfect);
-        // print_r("Ds");
-        }else{
-            $current_month_sent_to_finfect_ter_sum=0;
+        if (sizeof($data_sent_to_finfect) > 0) {
+            $current_month_sent_to_finfect_ter_sum = self::totalSum($data_sent_to_finfect);
+            // print_r("Ds");
+        } else {
+            $current_month_sent_to_finfect_ter_sum = 0;
         }
 
         // widget 3 =================
@@ -80,31 +80,30 @@ class HomeController extends Controller
         $data_sent_to_finfect = DB::table('tercouriers')->select('payable_amount')->where('status', 5)->whereMonth('updated_at', date("m"))->whereYear('updated_at', date("Y"))->get();
         // echo"<pre>";
         // print_r($data_sent_to_finfect);
-        if(sizeof($data_sent_to_finfect)>0){
-        $current_month_paid_ter_sum = self::totalSum($data_sent_to_finfect);
-        }else{
-            $current_month_paid_ter_sum=0;
+        if (sizeof($data_sent_to_finfect) > 0) {
+            $current_month_paid_ter_sum = self::totalSum($data_sent_to_finfect);
+        } else {
+            $current_month_paid_ter_sum = 0;
         }
 
         // =============== User Percentage ===============
-        $total_ter = Tercourier::select('id')->whereIn('status',[3,5])->whereMonth('updated_at', date("m"))->whereYear('updated_at', date("Y"))->count();
+        $total_ter = Tercourier::select('id')->whereIn('status', [3, 5])->whereMonth('updated_at', date("m"))->whereYear('updated_at', date("Y"))->count();
         // echo $total_ter; die;
-        
-        $user_array = array (
-            array("id" => 7, "name" =>'Vipin'),
-            array("id" => 9, "name" =>'Veena'),
-            array("id" => 10, "name" =>'Harpreet'),
-            array("id" => 11, "name" =>'Rameshwer')
-          );
-       
-        foreach($user_array as $key => $user){
-            
-            $user1_ter = Tercourier::select('id')->whereIn('status',[3,5])->where('updated_by_id', $user['id'])->whereMonth('updated_at', date("m"))->whereYear('updated_at', date("Y"))->count();
-           
-            $percentage[$user['name']][] = ($user1_ter / $total_ter) * 100 ;
-            
+
+        $user_array = array(
+            array("id" => 7, "name" => 'Vipin'),
+            array("id" => 9, "name" => 'Veena'),
+            array("id" => 10, "name" => 'Harpreet'),
+            array("id" => 11, "name" => 'Rameshwer'),
+        );
+
+        foreach ($user_array as $key => $user) {
+
+            $user1_ter = Tercourier::select('id')->whereIn('status', [3, 5])->where('updated_by_id', $user['id'])->whereMonth('updated_at', date("m"))->whereYear('updated_at', date("Y"))->count();
+
+            $percentage[$user['name']][] = ($user1_ter / $total_ter) * 100;
+
         }
-       
 
         return view('pages.dashboard', ['current_day_handover_ter_count' => $current_day_handover_ter_count, 'current_day_handover_ter_sum' => $current_day_handover_ter_sum,
             'current_month_handover_ter_count' => $current_month_handover_ter_count, 'current_month_handover_ter_sum' => $current_month_handover_ter_sum,
@@ -122,7 +121,7 @@ class HomeController extends Controller
 
         $sum1 = 0;
         $sum2 = array();
-        $sum3=0;
+        $sum3 = 0;
         $decode1 = json_decode($data);
         $decode2 = array();
         for ($i = 0; $i < sizeof($decode1); $i++) {
@@ -131,29 +130,28 @@ class HomeController extends Controller
         }
         // echo"<pre>";
         // print_r($decode2);
-        
+
         for ($j = 0, $n = 0; $j < sizeof($decode2); $j++) {
             // print_r(sizeof($decode2[$j]));
             // print_r($decode2[$j]);
             // print_r() . "<br>";
-            if(gettype($decode2[$j])== "array"){
-            if(!empty($decode2[$j])){
-            if (sizeof($decode2[$j]) > 1) {
-                $sum1 = array_sum($decode2[$j]);
-            } else {
-                $sum2[$n] = $decode2[$j][0];
-                $n++;
+            if (gettype($decode2[$j]) == "array") {
+                if (!empty($decode2[$j])) {
+                    if (sizeof($decode2[$j]) > 1) {
+                        $sum1 = array_sum($decode2[$j]);
+                    } else {
+                        $sum2[$n] = $decode2[$j][0];
+                        $n++;
 
+                    }
+
+                }
+            } else {
+                // echo"<pre>";
+                $sum3 = $sum3 + (int) $decode1[$j]->payable_amount;
             }
-            
         }
-         }
-         else{
-            // echo"<pre>";
-            $sum3=$sum3+(int)$decode1[$j]->payable_amount;
-         }
-        }
-        $current_month_sent_to_finfect_ter_sum = $sum1 + array_sum($sum2)+$sum3;
+        $current_month_sent_to_finfect_ter_sum = $sum1 + array_sum($sum2) + $sum3;
         // $current_month_sent_to_finfect_ter_sum = $sum1 + array_sum($sum2);
         return $current_month_sent_to_finfect_ter_sum;
     }
@@ -164,10 +162,9 @@ class HomeController extends Controller
 
         $current_ter_count = DB::table('tercouriers')->select('id')->where('status', 2)->whereMonth('updated_at', date("m"))->whereYear('updated_at', date("Y"))->count();
 
-        $previous_ter_count = DB::table('tercouriers')->select('id')->where('status', 2)->whereMonth('updated_at',$get_previous_month)->whereYear('updated_at', date("Y"))->count();
+        $previous_ter_count = DB::table('tercouriers')->select('id')->where('status', 2)->whereMonth('updated_at', $get_previous_month)->whereYear('updated_at', date("Y"))->count();
 
-        $old = DB::table('tercouriers')->select('id')->where('status', 2)->whereMonth('updated_at','!=',$get_previous_month)->whereMonth('updated_at','!=',date("m"))->whereYear('updated_at', date("Y"))->count();
-
+        $old = DB::table('tercouriers')->select('id')->where('status', 2)->whereMonth('updated_at', '!=', $get_previous_month)->whereMonth('updated_at', '!=', date("m"))->whereYear('updated_at', date("Y"))->count();
 
         $response['current_month_ter_count'] = $current_ter_count;
         $response['previous_ter_count'] = $previous_ter_count;
@@ -176,5 +173,176 @@ class HomeController extends Controller
         $response['messages'] = 'load_data';
 
         return Response::json($response);
+    }
+
+    public function dailyworkPerformance()
+    {
+        $total_ter = Tercourier::select('id')->whereIn('status', [3, 5])->whereMonth('updated_at', date("m"))->whereYear('updated_at', date("Y"))->count();
+
+        $user_array = array(
+            array("id" => 7, "name" => 'Vipin'),
+            array("id" => 9, "name" => 'Veena'),
+            array("id" => 10, "name" => 'Harpreet'),
+            array("id" => 11, "name" => 'Rameshwer'),
+        );
+
+        // ==========user 1 Vipin======
+        $users = Tercourier::select('id', 'updated_at')
+            ->where('updated_by_id', 7)
+            ->whereMonth('updated_at', date("m"))
+            ->get()
+            ->groupBy(function ($date) {
+                //return Carbon::parse($date->created_at)->format('Y'); // grouping by years
+                return Carbon::parse($date->updated_at)->format('d'); // grouping by months
+            });
+        $date = date('d');
+
+        $usermcount = [];
+        $userArr1 = [];
+
+        foreach ($users as $key => $value) {
+            $usermcount[(int) $key] = count($value);
+        }
+
+        for ($i = 1; $i <= $date; $i++) {
+            if (!empty($usermcount[$i])) {
+                $userArr1[$i] = $usermcount[$i];
+            } else {
+                $userArr1[$i] = 0;
+            }
+        }
+        $user1 = implode(',', $userArr1);
+        // echo'<pre>';print_r([$user1]); die;
+        // =========================
+        // ==========user 2 Veena======
+        $users = Tercourier::select('id', 'updated_at')
+            ->where('updated_by_id', 9)
+            ->whereMonth('updated_at', date("m"))
+            ->get()
+            ->groupBy(function ($date) {
+                //return Carbon::parse($date->created_at)->format('Y'); // grouping by years
+                return Carbon::parse($date->updated_at)->format('d'); // grouping by months
+            });
+        $date = date('d');
+
+        $usermcount = [];
+        $userArr2 = [];
+
+        foreach ($users as $key => $value) {
+            $usermcount[(int) $key] = count($value);
+        }
+
+        for ($i = 1; $i <= $date; $i++) {
+            if (!empty($usermcount[$i])) {
+                $userArr2[$i] = $usermcount[$i];
+            } else {
+                $userArr2[$i] = 0;
+            }
+        }
+        $user2 = implode(',', $userArr2);
+
+        // ==========user 3 Harpreet======
+        $users = Tercourier::select('id', 'updated_at')
+            ->where('updated_by_id', 10)
+            ->whereMonth('updated_at', date("m"))
+            ->get()
+            ->groupBy(function ($date) {
+                //return Carbon::parse($date->created_at)->format('Y'); // grouping by years
+                return Carbon::parse($date->updated_at)->format('d'); // grouping by months
+            });
+        $date = date('d');
+
+        $usermcount = [];
+        $userArr3 = [];
+
+        foreach ($users as $key => $value) {
+            $usermcount[(int) $key] = count($value);
+        }
+
+        for ($i = 1; $i <= $date; $i++) {
+            if (!empty($usermcount[$i])) {
+                $userArr3[$i] = $usermcount[$i];
+            } else {
+                $userArr3[$i] = 0;
+            }
+        }
+        $user3 = implode(',', $userArr3);
+
+        // ==========user 4 Rameshwer======
+        $users = Tercourier::select('id', 'updated_at')
+            ->where('updated_by_id', 11)
+            ->whereMonth('updated_at', date("m"))
+            ->get()
+            ->groupBy(function ($date) {
+                //return Carbon::parse($date->created_at)->format('Y'); // grouping by years
+                return Carbon::parse($date->updated_at)->format('d'); // grouping by months
+            });
+        $date = date('d');
+
+        $usermcount = [];
+        $userArr4 = [];
+
+        foreach ($users as $key => $value) {
+            $usermcount[(int) $key] = count($value);
+        }
+
+        for ($i = 1; $i <= $date; $i++) {
+            if (!empty($usermcount[$i])) {
+                $userArr4[$i] = $usermcount[$i];
+            } else {
+                $userArr4[$i] = 0;
+            }
+        }
+        $user4 = implode(',', $userArr4);
+        //////////////////////////
+        $countdate = array();
+        for ($i = 1; $i <= $date; $i++) {
+            $countdate[] = $i;
+        }
+        $countloop = implode(',', $countdate);
+
+        $response['user1'] = $user1;
+        $response['user2'] = $user2;
+        $response['user3'] = $user3;
+        $response['user4'] = $user4;
+        $response['countloop'] = $countloop;
+        $response['success'] = true;
+        $response['messages'] = 'load_data';
+
+        return Response::json($response);
+        ////////////////////////////
+        // foreach ($user_array as $key => $user) {
+
+        //     $data = Tercourier::select('updated_by_id', 'updated_at')
+        //         ->whereIn('status', ['3', '5'])
+        //         ->whereMonth('updated_at', date("m"))
+        //         ->whereYear('updated_at', date("Y"))
+        //         ->where('updated_by_id', $user['id'])
+        //         ->get();
+        //     $simple = json_decode(json_encode($data), true);
+        //     $dd[] = $simple;
+
+        // }
+        // $d = array();
+        // $c = 0;
+        // foreach ($dd as $key => $value) {
+        //     // echo'<pre>'; print_r($value); die;
+        //    //$d['ter_count'][] = count($value);
+        //     foreach ($value as $sub_key => $sub_val) {
+
+        //         //$d['date'][] = $sub_val['updated_at'];
+        //         $d[$sub_val['updated_by_id']]['date'][] = $sub_val['updated_at'];
+        //         $d[$sub_val['updated_by_id']]['count'][] = $sub_val['updated_at'];
+        //         //echo '<pre>';
+        //       //  print_r($sub_val);
+        //         //die;
+
+        //     }
+        //     $c++;
+        // }
+        // echo '<pre>';
+        // print_r($d);
+        // die;
+
     }
 }
--- public/js/dashboard-widget.js
+++ public/js/dashboard-widget.js
@@ -1,4 +1,4 @@
-// ============dashboard widgets
+// ============dashboard widgets percentage =================
 $(document).ready(function() {
 
     $.ajax({
@@ -116,3 +116,300 @@ $(document).ready(function() {
              });
  
  });
+//  ============================Daily Work Performance
+$(document).ready(function() {
+
+  $.ajax({
+               type: "GET",
+               url: "daily-work-performance",
+               success: function(response){
+
+                var vipin = response.user1.split(',');
+                var veena = response.user2.split(',');
+                var harpreet = response.user3.split(',');
+                var rameshwer = response.user4.split(',');
+
+                var month = response.countloop.split(',');
+
+
+                var options1 = {
+                  chart: {
+                    fontFamily: 'Nunito, sans-serif',
+                    height: 365,
+                    type: 'area',
+                    zoom: {
+                        enabled: false
+                    },
+                    dropShadow: {
+                      enabled: true,
+                      opacity: 0.2,
+                      blur: 10,
+                      left: -7,
+                      top: 22
+                    },
+                    toolbar: {
+                      show: false
+                    },
+                    events: {
+                      mounted: function(ctx, config) {
+                        const highest1 = ctx.getHighestValueInSeries(0);
+                        const highest2 = ctx.getHighestValueInSeries(1);
+                        const highest3 = ctx.getHighestValueInSeries(2);
+                        const highest4 = ctx.getHighestValueInSeries(3);
+                
+                        ctx.addPointAnnotation({
+                          x: new Date(ctx.w.globals.seriesX[0][ctx.w.globals.series[0].indexOf(highest1)]).getTime(),
+                          y: highest1,
+                          label: {
+                            style: {
+                              cssClass: 'd-none'
+                            }
+                          },
+                          customSVG: {
+                              SVG: '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#1b55e2" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>',
+                              cssClass: undefined,
+                              offsetX: -8,
+                              offsetY: 5
+                          }
+                        })
+                
+                        ctx.addPointAnnotation({
+                          x: new Date(ctx.w.globals.seriesX[1][ctx.w.globals.series[1].indexOf(highest2)]).getTime(),
+                          y: highest2,
+                          label: {
+                            style: {
+                              cssClass: 'd-none'
+                            }
+                          },
+                          customSVG: {
+                              SVG: '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#e7515a" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>',
+                              cssClass: undefined,
+                              offsetX: -8,
+                              offsetY: 5
+                          }
+                        })
+                        ctx.addPointAnnotation({
+                          x: new Date(ctx.w.globals.seriesX[2][ctx.w.globals.series[2].indexOf(highest3)]).getTime(),
+                          y: highest3,
+                          label: {
+                            style: {
+                              cssClass: 'd-none'
+                            }
+                          },
+                          customSVG: {
+                              SVG: '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#00ff00" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>',
+                              cssClass: undefined,
+                              offsetX: -8,
+                              offsetY: 5
+                          }
+                        })
+                
+                        ctx.addPointAnnotation({
+                          x: new Date(ctx.w.globals.seriesX[3][ctx.w.globals.series[3].indexOf(highest4)]).getTime(),
+                          y: highest4,
+                          label: {
+                            style: {
+                              cssClass: 'd-none'
+                            }
+                          },
+                          customSVG: {
+                              SVG: '<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="#ff00ff" stroke="#fff" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>',
+                              cssClass: undefined,
+                              offsetX: -8,
+                              offsetY: 5
+                          }
+                        })
+                        
+                      },
+                    }
+                  },
+                  colors: ['#1b55e2', '#e7515a','#00ff00','#ff00ff'],
+                  dataLabels: {
+                      enabled: false
+                  },
+                  markers: {
+                    discrete: [{
+                    seriesIndex: 0,
+                    dataPointIndex: 7,
+                    fillColor: '#000',
+                    strokeColor: '#000',
+                    size: 5
+                  }, {
+                    seriesIndex: 2,
+                    dataPointIndex: 11,
+                    fillColor: '#000',
+                    strokeColor: '#000',
+                    size: 4
+                  }]
+                  },
+                  subtitle: {
+                    text: '$10,840',
+                    align: 'left',
+                    margin: 0,
+                    offsetX: 95,
+                    offsetY: 0,
+                    floating: false,
+                    style: {
+                      fontSize: '18px',
+                      color:  '#4361ee'
+                    }
+                  },
+                  title: {
+                    text: 'Total Profit',
+                    align: 'left',
+                    margin: 0,
+                    offsetX: -10,
+                    offsetY: 0,
+                    floating: false,
+                    style: {
+                      fontSize: '18px',
+                      color:  '#0e1726'
+                    },
+                  },
+                  stroke: {
+                      show: true,
+                      curve: 'smooth',
+                      width: 2,
+                      lineCap: 'square'
+                  },
+                  series: [{
+                      name: 'Vipin',
+                      data: vipin
+                  }, 
+                  {
+                      name: 'Veena',
+                      data: veena
+                  },
+                  {
+                    name: 'Harpreet',
+                    data: harpreet
+                 },{
+                  name: 'Rameshwer',
+                  data: rameshwer
+               }
+                ],
+              
+                  labels: month,
+                  xaxis: {
+                    axisBorder: {
+                      show: false
+                    },
+                    axisTicks: {
+                      show: false
+                    },
+                    crosshairs: {
+                      show: true
+                    },
+                    labels: {
+                      offsetX: 0,
+                      offsetY: 5,
+                      style: {
+                          fontSize: '12px',
+                          fontFamily: 'Nunito, sans-serif',
+                          cssClass: 'apexcharts-xaxis-title',
+                      },
+                    }
+                  },
+                  yaxis: {
+                    labels: {
+                      formatter: function(value, index) {
+                        return value
+                      },
+                      offsetX: -22,
+                      offsetY: 0,
+                      style: {
+                          fontSize: '12px',
+                          fontFamily: 'Nunito, sans-serif',
+                          cssClass: 'apexcharts-yaxis-title',
+                      },
+                    }
+                  },
+                  grid: {
+                    borderColor: '#e0e6ed',
+                    strokeDashArray: 5,
+                    xaxis: {
+                        lines: {
+                            show: true
+                        }
+                    },   
+                    yaxis: {
+                        lines: {
+                            show: false,
+                        }
+                    },
+                    padding: {
+                      top: 0,
+                      right: 0,
+                      bottom: 0,
+                      left: -10
+                    }, 
+                  }, 
+                  legend: {
+                    position: 'top',
+                    horizontalAlign: 'right',
+                    offsetY: -50,
+                    fontSize: '16px',
+                    fontFamily: 'Nunito, sans-serif',
+                    markers: {
+                      width: 10,
+                      height: 10,
+                      strokeWidth: 0,
+                      strokeColor: '#fff',
+                      fillColors: undefined,
+                      radius: 12,
+                      onClick: undefined,
+                      offsetX: 0,
+                      offsetY: 0
+                    },    
+                    itemMargin: {
+                      horizontal: 0,
+                      vertical: 20
+                    }
+                  },
+                  tooltip: {
+                    theme: 'dark',
+                    marker: {
+                      show: true,
+                    },
+                    x: {
+                      show: false,
+                    }
+                  },
+                  fill: {
+                      type:"gradient",
+                      gradient: {
+                          type: "vertical",
+                          shadeIntensity: 1,
+                          inverseColors: !1,
+                          opacityFrom: .28,
+                          opacityTo: .05,
+                          stops: [45, 100]
+                      }
+                  },
+                  responsive: [{
+                    breakpoint: 575,
+                    options: {
+                      legend: {
+                          offsetY: -30,
+                      },
+                    },
+                  }]
+                }
+
+                /*
+      ================================
+          Revenue Monthly | Render
+      ================================
+  */
+  var chart1 = new ApexCharts(
+    document.querySelector("#revenueMonthly"),
+    options1
+);
+
+chart1.render();
+               }
+
+              });
+
+
+    });
