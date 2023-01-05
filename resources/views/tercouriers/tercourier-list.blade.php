7f1e56dd304a6775730a67d65b102d06fdd25d02
DhroovKanwar <dhroov.kanwar@eternitysolutions.net>
2023-01-05 11:17:21 +0530

    done changes for roles

--- resources/views/tercouriers/tercourier-list.blade.php
+++ resources/views/tercouriers/tercourier-list.blade.php
@@ -356,7 +356,7 @@
                                 <line x1="12" y1="15" x2="12" y2="3"></line>
                             </svg>
                         </button>
-                        @if ($role != 'reception')
+                        @if ($role != 'reception' || $name == 'Super Role')
                         <button class="actionButtons btn btn-success" @click="hand_shake_report()">
                             Handshake Report
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download">
