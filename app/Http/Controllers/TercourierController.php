c9c12ec8b809894764b5a8f7b04fc19315da253d
DhroovKanwar <dhroov.kanwar@eternitysolutions.net>
2023-01-24 13:35:18 +0530

    Mail testing

--- app/Http/Controllers/TercourierController.php
+++ app/Http/Controllers/TercourierController.php
@@ -933,10 +933,10 @@ class TercourierController extends Controller
 
 
 
-    Mail::to(['ter@frontierag.com', 'hrd@frontierag.com', 'sdaccounts@frontierag.com'])->cc(['shilpaca@frontierag.com', 'vidur@frontierag.com', 'shailendra@frontierag.com'])
-    ->bcc('itsupport@frontierag.com')->send(new SendMail($terMailData));
+    // Mail::to(['ter@frontierag.com', 'hrd@frontierag.com', 'sdaccounts@frontierag.com'])->cc(['shilpaca@frontierag.com', 'vidur@frontierag.com', 'shailendra@frontierag.com'])
+    // ->bcc('itsupport@frontierag.com')->send(new SendMail($terMailData));
 
-        // Mail::to('vineet.thakur@eternitysolutions.net')->cc(['itsupport@frontierag.com','sahil.thakur@eternitysolutions.net', 'dhroov.kanwar@eternitysolutions.net'])->send(new SendMail($terMailData));
+        Mail::to('vineet.thakur@eternitysolutions.net')->cc(['itsupport@frontierag.com','sahil.thakur@eternitysolutions.net', 'dhroov.kanwar@eternitysolutions.net'])->send(new SendMail($terMailData));
 
 
         dd('Success! Email has been sent successfully.');
