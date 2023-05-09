4e7d5f4de73d007f0fb700174dc9caa9bf3914ab
DhroovKanwar <dhroov.kanwar@eternitysolutions.net>
2023-05-09 11:40:59 +0530

    widgets commented for testing

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -440,7 +440,7 @@
 
          
 
-
+<!-- 
                 <div class="col-xl-4 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                 <div class="widget widget-chart-two">
                     <div class="widget-heading">
@@ -450,9 +450,9 @@
                         <div id="chart-2" class=""></div>
                     </div>
                 </div>
-            </div>
+            </div> -->
 
-            <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
+            <!-- <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                         <div class="widget widget-table-three">
 
                             <div class="widget-heading">
@@ -506,7 +506,7 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
               
             </div>
             @endif
