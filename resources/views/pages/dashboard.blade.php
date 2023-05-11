c8081e55f8e8d6d23895ca0ba807b6ffc7c295d9
DhruvKanwar <dhroov.kanwar@eternitysolutions.net>
2023-05-11 13:58:14 +0530

    test

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -451,7 +451,6 @@
                     </div>
                 </div>
             </div>
-
             <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
                         <div class="widget widget-table-three">
 
