78fa5c6a3c39c6d8ac45107d0f89f8ed5149ffcd
DhruvKanwar <dhroov.kanwar@eternitysolutions.net>
2023-05-24 13:31:47 +0530

    Single employee details get api

--- app/Http/Controllers/API/EmployeeController.php
+++ app/Http/Controllers/API/EmployeeController.php
@@ -26,4 +26,22 @@ class EmployeeController extends Controller
         }
         return Helper::apiResponseSend($message,$data,$status,$errorCode);
     }
+
+    public function getEmployeeDetail($emp_id) {
+        try{
+            $query = Sender::where('id',$emp_id)->select('id','name','employee_id','grade','status','personal_email_id')->get();
+            if($query){
+                $data = $query;
+                $message = "Employee fetched Successfully";
+                $status = true;
+                $errorCode = 200;
+            }
+        }catch(Exception $e) {
+            $data = '';
+            $message = "Invalid Record";
+            $status = false;
+            $errorCode = 401;
+        }
+        return Helper::apiResponseSend($message,$data,$status,$errorCode);
+    }
 }
