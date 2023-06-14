6dce5ef141707fc365681622d51dae6f62c46a33
DhruvKanwar <dhroov.kanwar@eternitysolutions.net>
2023-06-14 12:30:27 +0530

    changes reverted

--- public/js/form-validation.js
+++ public/js/form-validation.js
@@ -210,19 +210,8 @@ jQuery(document).ready(function () {
   
             },
             submitHandler: function (form) {
-                var errors = false;
-                $(this).find('.required').each(function () {
-                    if ($(this).val().length < 1) {
-                        $(this).addClass('error');
-                        errors = true;
-                    }
-                });
-                if (errors == true) {
-                    event.preventDefault();
-                }else{
                 document.getElementById('loadingBlock').style.display="flex";
                 formSubmitRedirect(form);
-                }
             },
         });
 
