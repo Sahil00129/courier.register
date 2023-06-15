8182fb64b5f6d2e77fd849cc63f134c7ae589a0f
DhruvKanwar <dhroov.kanwar@eternitysolutions.net>
2023-06-15 15:30:48 +0530

    widgets push

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -118,7 +118,7 @@
         @if ($role == 'admin' || $role == 'reception')
 
             {{-- ---for reception--- --}}
-            @if(false)
+            @if(true)
                 <div class="row flex-wrap layout-top-spacing">
                     <div
                         class="d-flex flex-column col-xl-4 col-lg-6 col-md-6 col-sm-6 col-12 layout-spacing dashboard-widget">
@@ -159,7 +159,7 @@
         @elseif($role=="ter user" || $role=="admin user" || $role=="hr admin")
             {{-- ----for HR & TER user---- --}}
 
-            @if(false)
+            @if(true)
             <div class="row layout-top-spacing">
                 <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-12 layout-spacing">
                     <div class="widget widget-four">
