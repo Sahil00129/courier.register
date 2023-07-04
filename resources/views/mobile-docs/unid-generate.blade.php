1ef233a10de06993ab812bb09993d46f5fef0573
DhruvKanwar <dhroov.kanwar@eternitysolutions.net>
2023-07-04 11:17:09 +0530

    Google Analytics

--- resources/views/mobile-docs/unid-generate.blade.php
+++ resources/views/mobile-docs/unid-generate.blade.php
@@ -20,7 +20,18 @@
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js" integrity="sha256-ngFW3UnAN0Tnm76mDuu7uUtYEcG3G5H1+zioJw3t+68=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/vee-validate@2.2.15/dist/vee-validate.min.js" integrity="sha256-m+taJnCBUpRECKCx5pbA0mw4ckdM2SvoNxgPMeUJU6E=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.js" integrity="sha256-bd8XIKzrtyJ1O5Sh3Xp3GiuMIzWC42ZekvrMMD4GxRg=" crossorigin="anonymous"></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-86MC6YTNHE"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-86MC6YTNHE');
+    </script>
 </head>
 
 <body>
@@ -992,14 +1003,13 @@
 
                                     if (this.tercourier_data != "" || this.tercourier_data != null) {
                                         var date_split;
-                                   
-                                        if(this.tercourier_data.tercourier != null)
-                                        {  
-                                         date_split = this.tercourier_data.tercourier.unid_generated_date.split("-");
-                                        }else{
+
+                                        if (this.tercourier_data.tercourier != null) {
+                                            date_split = this.tercourier_data.tercourier.unid_generated_date.split("-");
+                                        } else {
                                             date_split = this.tercourier_data.date_of_receipt.split("-");
                                         }
-                  
+
                                         let day, month_num, ter_date, ter_month_name;
                                         month_num = date_split[1];
                                         day = this.get_month_name(month_num);
